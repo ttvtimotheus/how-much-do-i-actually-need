381e02a908ea9a88341f55ed4597f15ebd10c0dd
ttvtimotheus <haseloff.timo@gmail.com>
2026-01-04 19:16:46 +0100

    Inital commit

---
+++ src/client/java/dev/hmdain/client/HowMuchDoIActuallyNeedClient.java
@@ -0,0 +1,10 @@
+package dev.hmdain.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HowMuchDoIActuallyNeedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
